9e45436279a20cca4f8ee19c8af6ca95156ea285
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2023-05-07 18:34:39 +0200

    r

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaDribble, FaPhone } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaDribble, FaPhone } from 'react-icons/fa';
 
 import { motion } from 'framer-motion';
 import { TypeAnimation } from 'react-type-animation';
@@ -7,23 +7,27 @@ import { fadeIn } from '../variants';
 import image from "../assets/beneface.jpg";
 
 const Banner = () => {
-  return <section className='section' id='home'>
+  return <section className='section min-h-[85vh] lg:min-h-[78vh]' id='home'>
     <div className="container mx-auto flex flex-wrap items-center">
       <div className="order-2 lg:order-1 lg:w-2/3 lg:mb-0 mb-6">
         <div className="blinkertxt lg:ml-5 ml-3">
-          <h1>Benedikt <span>Lantsoght</span></h1>
+          <h1 ><a className="titleName" target='blank' href="https://www.linkedin.com/in/benedikt-lantsoght-02481319/">Benedikt <span>&nbsp;Lantsoght&nbsp;</span>
+          
+          <FaLinkedin /> </a></h1> 
         </div>
         <div className="mb-6 text-[36px] lg:text-[60px]  noBlink">
           <span>&nbsp;</span>
           <TypeAnimation
             className="blinker font-semibold text-color"
-            sequence={['React Developer ', 2000, 'UX/UI Designer ', 1000, 'Multi(5)-Lingual ', 1000,  'T. 0499 388 227 ', 1000]}
+            sequence={['React Developer ', 2000, 'UX/UI Designer ', 1000, 'Multi(5)-Lingual ', 1000, 'T. 0499 388 227 ', 1000]}
             speed={50}
             wrap='span'
             repeat={Infinity}
             blink={false}
           />
+
         </div>
+
       </div>
       <div className="fotomeparent order-1 lg:order-2 lg:w-1/3">
         <div className="fotome max-w-xs rounded-full overflow-hidden mx-auto lg:ml-0 ml-3 m-10">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
-import cv from './cv.pdf';
+import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { FaDownload } from 'react-icons/fa';
-import Modal from './Modal';
 
 const Header = () => {
   const buttonRef = useRef(null);
-  const [downloaded, setDownloaded] = useState(false);
-  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     gsap.from(buttonRef.current, {
@@ -18,35 +14,12 @@ const Header = () => {
     });
   }, []);
 
-  const handleDownloadClick = () => {
-    setDownloaded(true);
-    setTimeout(() => {
-      setDownloaded(false);
-    }, 5000);
-  };
-
-  const handleModalOpen = () => {
-    setModalOpen(true);
-  };
-
-  const handleModalClose = () => {
-    setModalOpen(false);
-  };
-
   return (
     <div className="header">
-      <button href={cv} download id="myButton" ref={buttonRef} onClick={downloaded ? null : handleDownloadClick}>
-        {downloaded ? 'Downloaded' : (modalOpen ? 'Close Modal' : 'Open Modal')}
+      <button id="myButton" ref={buttonRef}>
+        <FaDownload /> &nbsp;CV
       </button>
-      {modalOpen && (
-        <Modal onClose={handleModalClose} size="70%">
-          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <button style={{ backgroundColor: 'transparent', border: 'none', color: 'red', cursor: 'pointer', fontSize: '24px' }} onClick={handleModalClose}>X</button>
-          </div>
-          <iframe src={cv} style={{ width: '100%', height: 'calc(100vh - 80px)' }} />
-        </Modal>
-      )}
-    </div>
+    </div>  
   );
 };
 
